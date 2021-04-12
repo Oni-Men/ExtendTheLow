d4cf532d4eb0b656b308f870ad23f32230576160
Oni-Men <sensyaheis@gmail.com>
2021-04-12 09:30:25 +0900

    Update SkillEnterCooltimeEvent.java

--- src/main/java/onim/en/etl/event/SkillEnterCooltimeEvent.java
+++ src/main/java/onim/en/etl/event/SkillEnterCooltimeEvent.java
@@ -1,20 +1,17 @@
 package onim.en.etl.event;
 
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import onim.en.etl.api.dto.SkillCooltimeResponse;
 
 public class SkillEnterCooltimeEvent extends Event {
 
-  private final long eventFiredWhen;
   private final long cooltimeEndsWhen;
   private final boolean isSpecialSkill;
   private SkillCooltimeResponse data;
 
   public SkillEnterCooltimeEvent(SkillCooltimeResponse data) {
     this.data = data;
-    eventFiredWhen = Minecraft.getSystemTime();
-    cooltimeEndsWhen = eventFiredWhen + data.cooltimeTick;
+    cooltimeEndsWhen = data.cooltimeEndsWhen;
     isSpecialSkill = data.skillType.equals("SPECIAL_SKILL");
   }
 
@@ -26,13 +23,9 @@ public class SkillEnterCooltimeEvent extends Event {
   public String getSkillName() {
     return this.data.skillName;
   }
-
-  public long getCooltimeTick() {
-    return this.data.cooltimeTick;
-  }
   
   public long getRemainingTicks() {
-    return cooltimeEndsWhen - eventFiredWhen;
+    return cooltimeEndsWhen - System.currentTimeMillis();
   }
 
   public String getSkillType() {
