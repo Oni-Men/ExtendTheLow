5a11c55aa3fb028c1153dd2bd07856b5681d313c
Oni-Men <sensyaheis@gmail.com>
2021-04-24 23:28:26 +0900

    PatchScoreNullError難読化対応

--- src/main/java/onim/en/etl/core/injector/PatchScoreNullError.java
+++ src/main/java/onim/en/etl/core/injector/PatchScoreNullError.java
@@ -16,21 +16,26 @@ import onim.en.etl.core.ObfuscateType;
 
 public class PatchScoreNullError extends HookInjector {
 
-  private static final String NET_HANDLER_PLAY_CLIENT =
+  private static final String PACKET_TEAMS =
       "net/minecraft/network/play/server/S3EPacketTeams";
 
+  private static final String OBF_PACKET_TEAMS = "hr";
+
   private static final String SCOREBOARD = "net/minecraft/scoreboard/Scoreboard";
+  private static final String OBF_SCOREBOARD = "auo";
 
   public PatchScoreNullError() {
     super("net.minecraft.client.network.NetHandlerPlayClient");
-    // TODO
-    super.registerEntry(ObfuscateType.NONE, "handleTeams", "(L" + NET_HANDLER_PLAY_CLIENT + ";)V");
+    super.registerEntry(ObfuscateType.NONE, "handleTeams", "(L" + PACKET_TEAMS + ";)V");
+    super.registerEntry(ObfuscateType.OBF, "a", "(L" + OBF_PACKET_TEAMS + ";)V");
   }
 
   @Override
   public boolean injectHook(InsnList list, ObfuscateType type) {
 
-    String desc = String.format("(L%s;L%s;)Z", NET_HANDLER_PLAY_CLIENT, SCOREBOARD);
+    String owner = type == ObfuscateType.NONE ? PACKET_TEAMS : OBF_PACKET_TEAMS;
+    String scoreboard = type == ObfuscateType.NONE ? SCOREBOARD : OBF_SCOREBOARD;
+    String desc = String.format("(L%s;L%s;)Z", owner, scoreboard);
     MethodInsnNode hook =
         new MethodInsnNode(Opcodes.INVOKESTATIC, HOOK, "onHandleTeams", desc, false);
 
